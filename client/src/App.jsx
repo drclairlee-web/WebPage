ead1edf1a6753205c5c923d5e78658330070ce2c
AmazingConnie <rmallu21@gmail.com>
2025-10-31 22:35:02 -0400

    revised home page using React-Bootstrap components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,54 +4,83 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios";
 import AILabHeader from './components/pagenavbar'
-import Container from 'react-bootstrap/esm/Container';
+import {Container, Row, Col, Image} from 'react-bootstrap';
 
 function App() {
+  /*
   const [count, setCount] = useState(0);
 
-  /*
+  
   const [array, setArray] = useState([]);
 
+  // Make a connect request to the backend
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api");
     setArray(response.data.names);
     console.log(response.data.names);
   }
-  /** Fetch list of names */
 
+  /** Fetch list of names */
   /*
   useEffect(() => {
     fetchAPI();
   }, []);
+  
   */
 
 
   return (
-    <>
+    <Container fluid>
+
       <AILabHeader/>
+      
+      <Container className='p-5'>
+           <Row>
+            <Col>
+                <h1 className='align-center'>Artificial Intelligence, Cybercrime, and Cybersecurity Research and Education Nexus (AI-CYREN) Lab</h1>
+                <br/>
+                <Image src="src/assets/ai-cyren-home-image.jpg" thumbnail/>
+            </Col>
+           </Row>
 
-      <section className='px-5 py-5'>
-        <h1>Artificial Intelligence, Cybercrime, and Cybersecurity Research and Education Nexus (AI-CYREN) Lab</h1>
-      </section>
+      </Container>
+  
+        <Container className='p-4'>
+          <Row>
+            <Col className='col-md-8'>
+              <p className='text-start fs-5'>The <b>AI-CYREN Lab</b> (Artificial Intelligence, Cybercrime, Cybersecurity, and Online Hate Research and Education Nexus) adopts a social science–driven approach to the study of cybersecurity and cybercrime. It seeks to advance interdisciplinary research and education at the intersection of technology and society. <br/> <br/> Through this integrative framework, the lab fosters a creative, holistic, and nuanced understanding of cyberattacks, cybercrime, cybersecurity, and AI phenomena.</p>
+            </Col>
+            <Col className='col-md-4 align-items-start'>
+              <Image src="src/assets/umllogo.svg" width={"250"} height={"250"} rounded/>
+            </Col>
+          </Row>
+        </Container>
+        
       
-
-      
-      
-      <section className='px-3 py-3'> 
-        <p className='text-start fs-5'>The AI-CYREN Lab (Artificial Intelligence, Cybercrime, Cybersecurity, and Online Hate Research and Education Nexus) adopts a social science–driven approach to the study of cybersecurity and cybercrime. It seeks to advance interdisciplinary research and education at the intersection of technology and society. <br/> <br/> Through this integrative framework, the lab fosters a creative, holistic, and nuanced understanding of cyberattacks, cybercrime, cybersecurity, and AI phenomena.</p>
-      </section>
-      
+        <footer>
+          <Container>
+            <p>Claire Lee 2025</p>
+          </Container>
+          
+        </footer>
+        
+        
         {
+          /** EXAMPLE: Mapping data from the backend to the front */
+
           /*
+          
+          
           array.map((name, index) => (
             <div key={index}>
-              <p>{name}</p>
+              <h1>{name}</h1>
               <br></br>
             </div>
           ))
           */
         }
-    </>
+      
+    </Container>
     
   )
 }
--- client/src/components/pagenavbar.jsx
+++ client/src/components/pagenavbar.jsx
@@ -1,23 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Image } from 'react-bootstrap';
 
 function AILabHeader() {
   return (
     <Navbar expand="lg" fixed="top" className="bg-body-tertiary" data-bs-theme="dark">
       <Container>
-        <img alt="umass lowell logo" src="src/assets/umllogo.svg" width='100' height='100' className='d-inline-block align-start'/>{' '}
+        <Image alt="umass lowell logo" src="src/assets/umllogo.svg" width='50' height='50' className='d-inline-block align-start'/>
         <Navbar.Brand href="#home">
-          
           AI-CYREN Lab
           </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Meet The Team</Nav.Link>
-            <Nav.Link href="#link">Research Projects</Nav.Link>
+            <Nav.Link href="#link">Research</Nav.Link>
             <Nav.Link href="#link">Publications</Nav.Link>
+            <Nav.Link href="#link">Team</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
